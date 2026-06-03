65c6f9ef270ebd10c62dac1896c2bcbe85ad5d97
Emin Gurbuz <emingurbuz@gmail.com>
2026-06-03 21:10:08 +0300

    Initial Commit

---
+++ Scrum FlashCards/Scrum_FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrum_FlashCardsApp.swift
+//  Scrum FlashCards
+//
+//  Created by Emin Gürbüz on 3.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Scrum_FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
